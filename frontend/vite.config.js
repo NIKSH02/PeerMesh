52533c79cc4002319cd2d34dd560614551aeadde
Nikhil Sharma <nikhilsharma0437@gmail.com>
2025-07-04 00:21:39 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 
-  server: {
-      allowedHosts: ['frontend.loca.lt'],
+  // server: {
+  //     allowedHosts: ['frontend.loca.lt'],
+  // },
+
+    build: {
+    chunkSizeWarningLimit: 1500,  // Size in KB (set higher as per your needs)
   },
 
   plugins: [react()],
